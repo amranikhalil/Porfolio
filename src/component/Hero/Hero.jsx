52cce3a9ef980dc7c2a3b2d27466bb69d2e93d72
amranikhalil <khalilamrani715@gmail.com>
2024-11-23 21:18:10 +0100

    gg

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -10,17 +10,17 @@ export const Hero = () => {
  
   useEffect(() => {
     const typedTitle = new Typed(elTitle.current, {
-      strings: ['Hi I\'m KHALIL, I am software <br> engineer specialised in web development'],
+      strings: ['Hi I\'m KHALIL, I am software engineer '],
       typeSpeed: 50,
       backSpeed: 40,
       showCursor:false
     });
-    // const typedDescription = new Typed(elDescription.current, {
-    //   strings: [' I am full stack developper with 2 years experience'],
-    //   typeSpeed: 50,
-    //   startDelay:4000,
-    //   showCursor:false
-    // });
+    const typedDescription = new Typed(elDescription.current, {
+      strings: [' specialised in web development'],
+      typeSpeed: 50,
+      startDelay:4000,
+      showCursor:false
+    });
 
     return () => {
       // Destroy Typed instance during cleanup to stop animation
